c54d0e5a3d529e7e48955514cb5c30c50df7e59a
Jean felipe <jeanfrs38@gmail.com>
2025-05-21 20:32:58 -0300

    feat: add CRUD

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Datas;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
     [ApiController]
     public class HomeController : ControllerBase
     {
-        [HttpGet("/")] 
-        
-        public string Get()
+        [HttpGet("/")]
+        public IActionResult Get([FromServices]AppDbContext context)
         {
-            return "Hello world!";
+            return Ok(context.Todos.ToList());
+        }
+
+        [HttpGet("/{id:int}")]
+        public IActionResult GetById([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var todos = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todos == null)
+                return NotFound();
+            return  Ok(todos);
+        }
+
+        [HttpPost("/")]
+        public IActionResult Post([FromBody]TodoModel todo, [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+            return Created($"/{todo.Id}", todo);
+        }
+
+        [HttpPut("/{id:int}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null) 
+                return NotFound();
+            
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("/{id:int}")]
+        public IActionResult Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var todo = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todo == null)
+                return NotFound();
+            
+            context.Todos.Remove(todo);
+            context.SaveChanges();
+
+            return Ok(todo);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using Todo.Datas;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<AppDbContext>();
 var app = builder.Build();
 
 app.MapControllers();
